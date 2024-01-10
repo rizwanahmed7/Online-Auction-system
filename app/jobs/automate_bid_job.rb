318ee14d7a6cb937da376975a611a0daecb62207
“rizwanahmed7” <code4ualways@gmail.comgit config --global user.name rizwanahmed7git config --global user.email code4ualways@gmail.com>
2024-01-10 14:47:14 -0500

    Job to automate bids

--- app/jobs/automate_bid_job.rb
+++ app/jobs/automate_bid_job.rb
@@ -3,4 +3,20 @@
 class AutomateBidJob < ApplicationJob
   queue_as :default
   RUN_EVERY = 30.seconds
+  
+  def perform(autobid, starting_amount)
+    byebug
+    maxbid = 0 if autobid.auctionproduct.bids.maximum(:bidamount).nil?
+    user_max_bid = autobid.auctionproduct.bids.where(user_id: autobid.user_id).maximum(:bidamount)
+    user_max_bid = 0 if user_max_bid.nil?
+    return unless user_max_bid != maxbid
+    return unless user_max_bid <= maxbid && autobid.amount > maxbid
+ 
+ 
+    starting_amount = maxbid + 1
+    bid = Bid.new(user_id: autobid.user_id, auctionproduct_id: autobid.auctionproduct_id,
+                  bidamount: starting_amount)
+    bid.save
+    WinnerJob.set(wait: 30.seconds).perform_later(autobid, starting_amount + 10)
+  end
 end
