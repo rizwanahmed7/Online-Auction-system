deb7b2beabc129123b60df9b7925889a1f20cebc
“rizwanahmed7” <code4ualways@gmail.comgit config --global user.name rizwanahmed7git config --global user.email code4ualways@gmail.com>
2024-01-10 15:25:29 -0500

    setup mailer

--- app/mailers/notifywinner_mailer.rb
+++ app/mailers/notifywinner_mailer.rb
@@ -1,26 +1,5 @@
 # frozen_string_literal: true
 
 class NotifywinnerMailer < ApplicationMailer
-  default from: 'syed.abdulrehman@devsinc.com'
-  SUBJECT = 'Bid Result'
-  MSG = 'Congratulations you won the bid !! :'
-  MSG2 = 'Sorry to inform you loose the bid !! :'
 
-  def bid_winner(user)
-    mail(
-      to: user.email,
-      body: "hellow #{user.name} #{MSG} ",
-      content: 'text/html',
-      subject: SUBJECT
-    )
-  end
-
-  def bid_looser(user)
-    mail(
-      to: user.email,
-      body: "hellow #{user.name} #{MSG2} ",
-      content: 'text/html',
-      subject: SUBJECT
-    )
-  end
 end
